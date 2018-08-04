11e2cf26c7372a84978db14516d1ed7a4f9fdddd
Amitava Das Gupta <amitavadg@gmail.com>
2018-08-04 11:52:49 +0530

    Requiring our own files.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-console.log('Starting the application.');
+console.log('Starting the app.js..');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
-
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) => {
-    if (err) {
-        console.log("Unable to write to file.");
-    }
-});
+var res = notes.add(5, 9);
+console.log(`The result of 5 + 9 is ${res}.`);
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} years old.`, (err) => {
+//     if (err) {
+//         console.log("Unable to write to file.");
+//     }
+// });
