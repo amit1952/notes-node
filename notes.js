0831b5725fb3f34b918cf05885b6256869bdfb3b
Amitava Das Gupta <amitavadg@gmail.com>
2018-08-05 10:55:14 +0530

    Using process.argv to get commandline args.

--- notes.js
+++ notes.js
@@ -5,3 +5,8 @@ module.exports.addNote = () => {
     return "New note";
 };
 
+let command = process.argv[2];
+console.log("Command:", command);
+
+if (command === "add") 
+    console.log("Add new note");
