64d036daef29867bfcf601e8b4918f2d34a03bc2
Amitava Das Gupta <amitavadg@gmail.com>
2018-08-05 12:30:50 +0530

    Getting input from the user.

--- notes.js
+++ notes.js
@@ -7,6 +7,16 @@ module.exports.addNote = () => {
 
 let command = process.argv[2];
 console.log("Command:", command);
+console.log(process.argv);
 
-if (command === "add") 
+if (command === "add") {
     console.log("Add new note");
+} else if (command === "list") {
+    console.log("Listing all notes");
+} else if (command === "read") {
+    console.log("Read a note");
+} else if (command === "remove") {
+    console.log("Removing a note");
+} else {
+    console.log("Command not recognized");
+}
