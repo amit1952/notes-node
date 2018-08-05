30bfe0d481d577ddb8b59ab438934f5d13432a02
Amitava Das Gupta <amitavadg@gmail.com>
2018-08-05 10:24:12 +0530

    Using nodemon instead of node to avoid restaring.

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 console.log('Starting the app.js..');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-var res = notes.add(5, 9);
-console.log(`The result of 5 + 9 is ${res}.`);
-// var user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} years old.`, (err) => {
-//     if (err) {
-//         console.log("Unable to write to file.");
-//     }
-// });
