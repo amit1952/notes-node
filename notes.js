16282681c91386e6ce388f688c18fdd4fb0f33fd
Amitava Das Gupta <amitavadg@gmail.com>
2018-08-13 17:34:12 +0530

    Write code to add ad save notes.

--- notes.js
+++ notes.js
@@ -1,7 +1,32 @@
 console.log('Starting notes.js...');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-    console.log("Adding note:", title, body);
+    var notes= [];
+    var note = {
+        title,
+        body
+    };
+
+    // Get Notes
+    try {
+        // Get existing notes into the array
+        let notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+        
+    }
+
+    const duplicateNotes = notes.filter((note) => note.title === title);
+
+    // Save Notes
+    if (duplicateNotes.length === 0) {
+        // Add new note to the notes array and write it to the file.
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+
 };
 
 var getAll = () => {
