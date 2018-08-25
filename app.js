d4c9bd50cba1ce7cf84d9d65b5ad6998873a59a3
Amitava Das Gupta <amitavadg@gmail.com>
2018-08-25 15:46:33 +0530

    Refactor addNote and other parts.

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ console.log("Command:", command);
 console.log("Yargs:", argv);
 
 if (command === "add") {
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+    if (note) {
+        console.log("The note was created successfully");
+        console.log('---');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    } else {
+        console.log("Failed to create new note. \nPerhaps a duplicate title exists");
+    }
 } else if (command === "list") {
     notes.getAll();
 } else if (command === "read") {
--- notes.js
+++ notes.js
@@ -2,42 +2,49 @@ console.log('Starting notes.js...');
 
 const fs = require('fs');
 
-var addNote = (title, body) => {
-    var notes= [];
-    var note = {
-        title,
-        body
-    };
-
+const fetchNotes = () => {
     // Get Notes
     try {
         // Get existing notes into the array
         let notesString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesString);
-    } catch (e) {
-        
+        return JSON.parse(notesString);
+    } catch (e) {   // if no notes exist or invalid data
+        return [];
     }
+};
 
+const saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+const addNote = (title, body) => {
+    let notes = fetchNotes();
+    let note = {
+        title,
+        body
+    };
+    // Check for duplicates
     const duplicateNotes = notes.filter((note) => note.title === title);
 
     // Save Notes
     if (duplicateNotes.length === 0) {
         // Add new note to the notes array and write it to the file.
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        saveNotes(notes);
+        return note;
     }
 
 };
 
-var getAll = () => {
+const getAll = () => {
     console.log("Getting all notes.");
 };
 
-var readNote = (title) => {
+const readNote = (title) => {
     console.log("Reading note with title:", title);
 };
 
-var removeNote = (title) => {
+const removeNote = (title) => {
     console.log("Removing note with title:", title);
 };
 
